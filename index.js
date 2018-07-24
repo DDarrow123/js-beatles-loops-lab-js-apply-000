844a730dd4830f52cc91c496ddae262765ecf348
Devon Darrow <ddarrowphl@gmail.com>
2018-07-24 02:24:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 function theBeatlesPlay(musicians, instruments){
   var emptyArray = [];
-  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-  //var i;
   for (i = 0; i < musicians.length; i++) {
     emptyArray += musicians[i] + " plays " + instruments[i];
   }
