ddb4e66038e1015dd859ce8aa88c80dd00d70f80
Devon Darrow <ddarrowphl@gmail.com>
2018-07-24 02:19:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = "";
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  //var i;
+  for (i = 0; i < musicians.length; i++) {
+    for (j = 0; j < instruments.length; i++){
+      if (musicians[i] = instruments[j]) {
+        emptyArray += musicians[i] + instruments[j];
+      }
+    }
+  }
+
+
+}
+
+
+
 // add solution here
