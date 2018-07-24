44ec641f15bf6d78fc50a392ec47626ae49ae0e2
Devon Darrow <ddarrowphl@gmail.com>
2018-07-24 02:26:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments){
   var i;
   var emptyArray = [];
   for (i = 0; i < musicians.length; i++) {
-    emptyArray += musicians[i] + " plays " + instruments[i];
+    emptyArray.push(musicians[i] + " plays " + instruments[i]);
   }
   return emptyArray;
 }
